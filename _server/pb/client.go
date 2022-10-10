364af84570c0b07db2e433ece9c52e1fe23b05cc
axiaoxin <254606826@qq.com>
2022-10-10 17:30:55 +0800

    dial with block

--- _server/pb/client.go
+++ _server/pb/client.go
@@ -1,6 +1,8 @@
 package pb
 
 import (
+	"time"
+
 	"github.com/axiaoxin-com/logging"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -16,9 +18,10 @@ func InitGrpcClient(addr string) {
 	if GrpcClient != nil {
 		return
 	}
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
 	if err != nil {
-		logging.Fatal(nil, err.Error())
+		logging.Fatal(nil, "dial vcode failed:"+err.Error())
 	}
 	GrpcClient = NewVcodeServiceClient(conn)
+	logging.Debug(nil, "dial vcode success on:"+addr)
 }
